bf38e4e552e8b4a0522fa835b6d735955add48b3
xolesteroll <xol2dem@gmail.com>
2020-09-25 14:19:39 +0300

    embed try

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ let gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     clean = require('del'),
-    autoprefixer = require('gulp-autoprefixer')
+    autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('clean', async function() {
-    clean.sync('dist')
+gulp.task('clean', async function () {
+    clean.sync('dist');
 });
 
 
-gulp.task('scss', function() {
+gulp.task('scss', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer({
@@ -20,41 +20,41 @@ gulp.task('scss', function() {
         }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
     return gulp.src([
-            'node_modules/normalize.css/normalize.css',
-            'node_modules/slick-carousel/slick/slick.css',
-        ])
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+    ])
         .pipe(concat('_libs.scss'))
         .pipe(gulp.dest('app/scss'))
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
     return gulp.src('app/*.html')
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('script', function() {
+gulp.task('script', function () {
     return gulp.src('app/js/*.js')
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
     return gulp.src([
-            'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/slick-carousel/slick/slick.js',
 
-        ])
+    ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('app/js'))
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "app/"
@@ -62,24 +62,27 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('export', async function() {
+gulp.task('export', async function () {
     let buildHtml = gulp.src('app/**/*.html')
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 
     let buildCss = gulp.src('app/css/**/*.css')
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('dist/css'));
 
     let buildJs = gulp.src('app/js/**/*.js')
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('dist/js'));
 
     let buildFonts = gulp.src('app/fonts/**/*.*')
-        .pipe(gulp.dest('dist/fonts'))
+        .pipe(gulp.dest('dist/fonts'));
 
     let buildImg = gulp.src('app/images/**/*.*')
-        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest('dist/images'));
+
+    let buildAudio = gulp.src('app/audio/**/*.*')
+        .pipe(gulp.dest('dist/audio'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('script'))
